bc68fc00fb2d5b87bae84cb64ee3db03c5a30435
Wolfie Waffle <wolfie.waffle11@gmail.com>
2022-06-17 17:42:15 -0400

    Make torches burn out in water

--- src/main/java/com/github/wolfiewaffle/hardcore_torches/mixin/InventoryTickMixin.java
+++ src/main/java/com/github/wolfiewaffle/hardcore_torches/mixin/InventoryTickMixin.java
@@ -1,6 +1,7 @@
 package com.github.wolfiewaffle.hardcore_torches.mixin;
 
 import com.github.wolfiewaffle.hardcore_torches.Mod;
+import com.github.wolfiewaffle.hardcore_torches.config.HardcoreTorchesConfig;
 import com.github.wolfiewaffle.hardcore_torches.item.LanternItem;
 import com.github.wolfiewaffle.hardcore_torches.item.TorchItem;
 import com.github.wolfiewaffle.hardcore_torches.util.ETorchState;
@@ -8,7 +9,10 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -28,8 +32,9 @@ public abstract class InventoryTickMixin {
     @Inject(at = @At("TAIL"), method = "tick")
     private void tick(CallbackInfo info) {
         if (!getWorld().isClient) {
+            ServerPlayerEntity player = ((ServerPlayerEntity) (Object) this);
 
-            PlayerInventory inventory = ((ServerPlayerEntity) (Object) this).getInventory(); //idek man, Commoble told me to do it
+            PlayerInventory inventory = player.getInventory(); //idek man, Commobile told me to do it
 
             for (int i = 0; i < inventory.offHand.size(); i++) {
                 tickTorch(inventory.offHand.get(i), inventory, i, inventory.offHand);
@@ -37,6 +42,61 @@ public abstract class InventoryTickMixin {
 
             for (int i = 0; i < inventory.main.size(); i++) {
                 tickTorch(inventory.main.get(i), inventory, i, inventory.main);
+            }
+
+            waterCheck(player, inventory);
+        }
+    }
+
+    private void waterCheck(ServerPlayerEntity player, PlayerInventory inventory) {
+        BlockPos pos = player.getBlockPos();
+        int rainEffect = Mod.config.invExtinguishInRain;
+        boolean doRain = rainEffect > 0 && player.getWorld().hasRain(pos);
+
+        for (int i = 0; i < inventory.size(); i++) {
+            ItemStack stack = inventory.getStack(i);
+            Item item = stack.getItem();
+
+            // Torches
+            if (item instanceof TorchItem) {
+                TorchItem torchItem = (TorchItem) item;
+                boolean rain = doRain;
+                boolean mainOrOffhand = (i == inventory.selectedSlot || inventory.offHand.get(0) == stack);
+                if (rainEffect == 1 && doRain) rain = mainOrOffhand ? true : false;
+
+                // Rain
+                if (rain) rainTorch(i, torchItem, stack, inventory, player.getWorld(), pos);
+
+                // Underwater
+                if (Mod.config.invExtinguishInWater > 0) waterTorch(i, torchItem, stack, player, mainOrOffhand, pos);
+            }
+        }
+    }
+
+    private void waterTorch(int i, TorchItem torchItem, ItemStack stack, ServerPlayerEntity player, boolean mainOrOffhand, BlockPos pos) {
+        if (player.isSubmergedInWater()) {
+            if (torchItem.getTorchState() == ETorchState.LIT || torchItem.getTorchState() == ETorchState.SMOLDERING) {
+                if ((Mod.config.invExtinguishInWater == 1 && mainOrOffhand) || Mod.config.invExtinguishInWater == 2) {
+                    player.getInventory().setStack(i, TorchItem.stateStack(stack, ETorchState.UNLIT));
+                    player.getWorld().playSound(null, pos.up(), SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.PLAYERS, 0.5f, 1f);
+                }
+            }
+        }
+    }
+
+    private void rainTorch(int i, TorchItem torchItem, ItemStack stack, PlayerInventory inventory, World world, BlockPos pos) {
+        if (torchItem.getTorchState() == ETorchState.LIT) {
+            if (Mod.config.torchesSmolder) {
+                inventory.setStack(i, TorchItem.stateStack(stack, ETorchState.SMOLDERING));
+                world.playSound(null, pos.up(), SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.PLAYERS, 0.5f, 1f);
+            } else {
+                inventory.setStack(i, TorchItem.stateStack(stack, ETorchState.UNLIT));
+                world.playSound(null, pos.up(), SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.PLAYERS, 0.5f, 1f);
+            }
+        } else if (torchItem.getTorchState() == ETorchState.SMOLDERING) {
+            if (!Mod.config.torchesSmolder) {
+                inventory.setStack(i, TorchItem.stateStack(stack, ETorchState.UNLIT));
+                world.playSound(null, pos.up(), SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.PLAYERS, 0.5f, 1f);
             }
         }
     }
